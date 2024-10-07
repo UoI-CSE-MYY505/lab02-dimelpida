bde2dccc27db0d7006c1c20427dd056a26fadd7e
ElpidaDim <cs04867@uoi.gr>
2024-10-07 15:47:42 +0300

    Completed lab02

--- lab02.s
+++ lab02.s
@@ -13,6 +13,28 @@ prog:
 # Write your code here!
 # Do not remove the prog label or write code above it!
 #-----------------------------
+
+    add s0 , zero , zero #arxikopoihsh tou s0=0 
+    add t2, zero, zero #t2=0 
+loop: 
+    beq a1, t2, done #an t2=a1 termatizei to loop
+    
+    slli t1,t2,2 #t1=t2 * 4 
+    add t1,t1,a0 #ypologizw th diefthinsi toy stoixeiou
+    
+    #fortwsi timis ston t3
+    lw t3, 0(t1) #fortosi ths leksis pou h diefthinsi ths einai ston t1
+
+    #elegxos an h timh tou stoixeiou ston t3 einai ish me a2
+    bne t3, a2, label_next #an t3!=a2 phgaine sthn etiketa label_next
+      
+    #an t3=a2 
+    add s0,t1,zero #apothikeuoume th diefthinsi tou t1 ston s0
+
+label_next: 
+    #auksanoume ton deikti 
+    addi t2, t2, 1
+    j loop # epistrofh sthn arxi tou loop
 done:
     addi a7, zero, 10 
     ecall
